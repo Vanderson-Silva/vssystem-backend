e88df6d2ad61103b1eff623108a079cce25a6e48
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-04-16 15:29:58 -0300

    Criando a Propriedades para o Banco de Dados H2

--- src/main/java/com/vanderson/vssystembackend/VssystemBackendApplication.java
+++ src/main/java/com/vanderson/vssystembackend/VssystemBackendApplication.java
@@ -3,11 +3,14 @@ package com.vanderson.vssystembackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class VssystemBackendApplication {
+public class VssystemBackendApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VssystemBackendApplication.class, args);
 	}
+
 
 }
