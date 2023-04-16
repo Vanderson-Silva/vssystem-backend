39c7b64badd2d11fa613fe1f48eadc9af20fbc79
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-04-16 15:02:27 -0300

    Iniciando o Projeto

---
+++ src/main/java/com/vanderson/vssystembackend/VssystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vanderson.vssystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VssystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VssystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanderson/vssystembackend/VssystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanderson.vssystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VssystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
