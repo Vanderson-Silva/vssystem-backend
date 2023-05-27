1cad0892dbd11cca58aefb9fcd38df8c20376485
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-05-26 21:13:57 -0300

    Adicionando Anotaçoes do Lombok na Classe Cliente

--- src/main/java/com/vanderson/vssystembackend/model/Cliente.java
+++ src/main/java/com/vanderson/vssystembackend/model/Cliente.java
@@ -1,16 +1,19 @@
 package com.vanderson.vssystembackend.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
+
 
 @Entity
+@NoArgsConstructor
+@Data
 public class Cliente implements Serializable {
     private static final long SerialVersionUID = 1L;
     @Id
@@ -27,10 +30,6 @@ public class Cliente implements Serializable {
     private String telefone;
     private String celular;
 
-
-    public Cliente() {
-        super();
-    }
 
     public Cliente(Long id, String nome, Date dataCadastro,
                    String status, String endereco, String numero,
@@ -49,98 +48,4 @@ public class Cliente implements Serializable {
     }
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
-    public Date getDataCadastro() {
-        return dataCadastro;
-    }
-
-    public void setDataCadastro(Date dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cliente cliente = (Cliente) o;
-        return id.equals(cliente.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
