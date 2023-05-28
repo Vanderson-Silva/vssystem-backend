8caf262f02329444dbf072ea4e6ee8b03f70557f
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-05-28 20:28:58 -0300

    Criando a Classe Fornecedor Service

---
+++ src/main/java/com/vanderson/vssystembackend/service/ProdutoService.java
@@ -0,0 +1,58 @@
+package com.vanderson.vssystembackend.service;
+
+
+import com.vanderson.vssystembackend.model.Produto;
+import com.vanderson.vssystembackend.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    // Metodo de busca pelo Id
+    public Produto findById(Long id) {
+        Optional<Produto> obj = produtoRepository.findById(id);
+        return obj.orElse(null);
+    }
+
+
+    // metodo para listar todos os produtos.
+    public List<Produto> listarTodosFornecedores() {
+        List<Produto> list = produtoRepository.findAll();
+        return list;
+    }
+
+    // metodo para excluir um produto
+    public void delete(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    //metodo para salvar um produto
+    public Produto create(Produto obj) {
+        obj.setId(null);
+        return produtoRepository.save(obj);
+    }
+
+    // metodo para editar um cliente
+    public Produto update(Long id, Produto obj) {
+        Produto produto = findById(id);
+        produto.setReferencia(obj.getReferencia());
+        produto.setDescricao(obj.getDescricao());
+        produto.setDataCadastro(obj.getDataCadastro());
+        produto.setStatus(obj.getStatus());
+        produto.setQtdmlprod(obj.getQtdmlprod());
+        produto.setValorCompra(obj.getValorCompra());
+        produto.setValorVenda(obj.getValorVenda());
+        produto.setFornecedor(obj.getFornecedor());
+        produto.setQdtEstoque(obj.getQdtEstoque());
+        return produtoRepository.save(obj);
+
+
+    }
+}
